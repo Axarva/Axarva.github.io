cae3ee4d1baf89950400ec1c002e4eef0872c7c6
Axarva <timsinaatharva@gmail.com>
2020-08-24 18:04:00 +0545

    Make gallery functional.

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,28 @@
 function activateGallery() {
-    alert("Why are you here, pleb?");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo > img");
+    let imageHead = document.querySelector("#gallery-info > h3");
+    let imagePara = document.querySelector("#gallery-info > p");
+    let currentClass = "current";
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener("click", function() {
+            //Changes the selected image borders.
+            document.querySelector(".current").classList.remove(currentClass);
+            thumbnail.parentElement.classList.add(currentClass);
+
+            //Changes the main image.
+            setTimeout(() => {
+                let imgSrc = thumbnail.dataset.largeVersion;
+                mainImage.setAttribute("src", imgSrc);
+                mainImage.setAttribute("alt", thumbnail.alt);
+    
+                //Changes the image info in Column 3.
+                let imgTitle = thumbnail.dataset.title;
+                let imgDescription = thumbnail.dataset.description;
+                imageHead.innerHTML = imgTitle;
+                imagePara.innerHTML = imgDescription;
+            }, 200);
+        })
+    })
+    
 }
