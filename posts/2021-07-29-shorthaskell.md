afc4696e2322e9c63d0aa81478ff9fa1e368ab9a
axarva <timsina@axarva.me>
2026-01-16 17:51:38 +0900

    New website who dis

--- posts/2021-07-29-shorthaskell.md
+++
@@ -1,8 +0,0 @@
----
-title: Short Haskell Scripts
-github: https://github.com/Axarva/haskell-stuff
----
-
-A bunch of short Haskell scripts with various functions.
-This is basically my set of solutions to the work in Coursera's DSA specialization.
-
--- site.hs
+++ site.hs
@@ -2,8 +2,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-import           Text.Pandoc
-
 
 --------------------------------------------------------------------------------
 config :: Configuration
@@ -13,54 +11,48 @@ config = defaultConfiguration
 
 main :: IO ()
 main = hakyllWith config $ do
-    match "static/*/*" $ do
-        route idRoute
+    match "images/*" $ do
+        route   idRoute
         compile copyFileCompiler
 
-    match (fromList ["about/index.md", "contact/index.md"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/page.html" siteCtx
-            >>= loadAndApplyTemplate "templates/default.html" siteCtx
-            >>= relativizeUrls
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            -- >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["profile/index.html"] $ do
+    create ["blog.html"] $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Profile"             `mappend`
+                    constField "title" "Thoughts"            `mappend`
                     siteCtx
 
             makeItem ""
-                >>= loadAndApplyTemplate "templates/profile.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
-
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    siteCtx
-
             getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= applyAsTemplate siteCtx
+                >>= loadAndApplyTemplate "templates/default.html" siteCtx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
+    match "resume.pdf" $ do
+        route idRoute
+        compile copyFileCompiler
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
@@ -70,8 +62,5 @@ postCtx =
 
 siteCtx :: Context String
 siteCtx =
-    constField "baseurl" "http://localhost:8000" `mappend`
-    constField "site_description" "You've landed on my website" `mappend`
-    constField "instagram_username" "atharva_timsina" `mappend`
-    constField "github_username" "Axarva" `mappend`
+    constField "baseurl" "https://axarva.me" `mappend` 
     defaultContext
